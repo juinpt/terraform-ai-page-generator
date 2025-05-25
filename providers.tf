1a5ad464e8ea7b47eef067685acfc291cd916d1f
Joao Afonso <joao@afonso.jp>
2025-05-25 23:36:51 +0900

    removed hardcoded profile used with my AWS CLI

--- providers.tf
+++ providers.tf
@@ -1,4 +1,6 @@
 provider "aws" {
-  profile = "default"
-  region  = "ap-northeast-1"
+# This is only used for with my AWS CLI config
+#  profile = "default"
+#  region  = "ap-northeast-1"
+  region  = var.aws_region
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "openai_api_key" {
   type      = string
   sensitive = true
 }
+
+variable "aws_region" {
+  type        = string
+  default     = "ap-northeast-1"
+  description = "AWS Region"
+}
